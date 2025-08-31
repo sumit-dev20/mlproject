eca59069719cf32d6df04ce3ce9dd477b40eac5b
sumitacharya206 <sumitacharya294@gmail.com>
2025-08-31 08:32:17 +0530

    few changes

---
+++ src/data/data_ingestion.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from dataclasses import dataclass
+import os
+from src.utils.exception import CustomException
+from src.utils.logger import logging
+import sys
+from sklearn.model_selection import train_test_split
+from src.data.data_transformation import DataTransformation
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method")
+        try:
+            df = pd.read_csv("notebooks/data/stud.csv")
+
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True
+            )
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path, index=False, header=True
+            )
+            test_set.to_csv(
+                self.ingestion_config.test_data_path, index=False, header=True
+            )
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+        except Exception as e:
+            CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_path, test_path = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(
+        train_path=train_path, test_path=test_path
+    )
---
+++ src/utils/utils.py
@@ -0,0 +1,17 @@
+import os
+from src.utils.exception import CustomException
+import sys
+import dill
+
+
+def save_object(file_path: str, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
